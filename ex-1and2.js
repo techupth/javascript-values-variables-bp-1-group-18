2f95c19e330a83bf39b8473ca936852dfb383ec9
Burahan <burahan206@example.com>
2023-09-18 21:39:17 +0700

    feat:Values&Variables assignment

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,14 @@
 //Exercise #1: Variable Creation
-//Start coding here
-
-//Exercise #2: Variable Reassignment
-//Start coding here
+// Exercise #1: Variable Creation 
+// Start coding here 
+let name = "karter"
+let surname = "anderson"
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
+const restaurantName ="Chef's Table by Chef James"
+const netFoodPrice = 15000
+const numberOfRestaurant = 1500
+// Exercise #2: Variable Reassignment
+// Start coding here
+address ="(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+ netFoodPrice = 5000
+ numberOfRestaurant = 2500
--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,4 @@
 // Exercise #3: Celsius to Fahrenheit
+
 let celsius = 30;
-
-// Start coding here;
-
-let fahrenheit;
+let fahrenheit = (celsius * 1.8) + 32
